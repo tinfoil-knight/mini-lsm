071200ff33903f322278cfae4d31426c6bfc00f9
Kunal Kundu <kunal99kundu@gmail.com>
2024-08-12 21:21:23 +0530

    week1, day3, task1

--- mini-lsm-starter/src/block.rs
+++ mini-lsm-starter/src/block.rs
@@ -1,6 +1,3 @@
-#![allow(unused_variables)] // TODO(you): remove this lint after implementing this mod
-#![allow(dead_code)] // TODO(you): remove this lint after implementing this mod
-
 mod builder;
 mod iterator;
 
@@ -18,11 +15,55 @@ impl Block {
     /// Encode the internal data to the data layout illustrated in the tutorial
     /// Note: You may want to recheck if any of the expected field is missing from your output
     pub fn encode(&self) -> Bytes {
-        unimplemented!()
+        // Block encoding format
+        // ----------------------------------------------------------------------------------------------------
+        // |             Data Section             |              Offset Section             |      Extra      |
+        // ----------------------------------------------------------------------------------------------------
+        // | Entry #1 | Entry #2 | ... | Entry #N | Offset #1 | Offset #2 | ... | Offset #N | num_of_elements |
+        // ----------------------------------------------------------------------------------------------------
+        //
+        // Each entry is a key-value pair.
+        // -----------------------------------------------------------------------
+        // |                           Entry #1                            | ... |
+        // -----------------------------------------------------------------------
+        // | key_len (2B) | key (keylen) | value_len (2B) | value (varlen) | ... |
+        // -----------------------------------------------------------------------
+        //
+        // -------------------------------
+        // |offset|offset|num_of_elements|
+        // -------------------------------
+        // |   0  |  12  |       2       |
+        // -------------------------------
+        // The footer of the block will be as above. Each of the number is stored as u16.
+
+        let num_elements = self.offsets.len();
+        let offsets: Vec<u8> = self.offsets.iter().flat_map(|x| x.to_le_bytes()).collect();
+        Bytes::from(
+            [
+                self.data.as_slice(),
+                &offsets,
+                &(num_elements as u16).to_le_bytes(),
+            ]
+            .concat(),
+        )
     }
 
     /// Decode from the data layout, transform the input `data` to a single `Block`
     pub fn decode(data: &[u8]) -> Self {
-        unimplemented!()
+        let (start, end) = (data.len() - 2, data.len());
+        let num_elements = u16::from_le_bytes([data[start], data[end - 1]]);
+        let offset_size = (num_elements * 2) as usize; // since offset is u16
+        let (start, end) = (start - offset_size, start);
+        let offsets: Vec<u16> = data[start..end]
+            .chunks_exact(2)
+            .map(|chunk| u16::from_le_bytes([chunk[0], chunk[1]]))
+            .collect();
+
+        let (start, end) = (0, start);
+
+        Self {
+            data: data[start..end].to_vec(),
+            offsets,
+        }
     }
 }
--- mini-lsm-starter/src/block/builder.rs
+++ mini-lsm-starter/src/block/builder.rs
@@ -1,6 +1,3 @@
-#![allow(unused_variables)] // TODO(you): remove this lint after implementing this mod
-#![allow(dead_code)] // TODO(you): remove this lint after implementing this mod
-
 use crate::key::{KeySlice, KeyVec};
 
 use super::Block;
@@ -20,22 +17,55 @@ pub struct BlockBuilder {
 impl BlockBuilder {
     /// Creates a new block builder.
     pub fn new(block_size: usize) -> Self {
-        unimplemented!()
+        Self {
+            offsets: Vec::new(),
+            data: Vec::new(),
+            block_size,
+            first_key: KeyVec::new(),
+        }
     }
 
     /// Adds a key-value pair to the block. Returns false when the block is full.
     #[must_use]
     pub fn add(&mut self, key: KeySlice, value: &[u8]) -> bool {
-        unimplemented!()
+        let (key_len, value_len) = (key.len(), value.len());
+        let current_block_size = self.data.len() + self.offsets.len() * 2;
+        let increase = 2 + key_len + 2 + value_len + 2;
+        let expected_block_size = current_block_size + increase + 2;
+
+        let is_first = self.is_empty();
+        if expected_block_size > self.block_size && !is_first {
+            return false;
+        }
+        self.offsets.push(if is_first {
+            0
+        } else {
+            current_block_size as u16 - 1
+        });
+        let pair = [
+            &(key_len as u16).to_le_bytes(),
+            key.into_inner(),
+            &(value_len as u16).to_le_bytes(),
+            value,
+        ]
+        .concat();
+        self.data.extend(pair);
+        if is_first {
+            self.first_key = KeyVec::from_vec(key.into_inner().to_vec());
+        }
+        true
     }
 
     /// Check if there is no key-value pair in the block.
     pub fn is_empty(&self) -> bool {
-        unimplemented!()
+        self.data.is_empty()
     }
 
     /// Finalize the block.
     pub fn build(self) -> Block {
-        unimplemented!()
+        Block {
+            data: self.data,
+            offsets: self.offsets,
+        }
     }
 }
