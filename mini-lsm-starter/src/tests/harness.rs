9115d6dcd3bb2938544f0f9aca4fccee7ad8875d
Kunal Kundu <kunal99kundu@gmail.com>
2024-08-25 21:18:45 +0530

    update tests

--- mini-lsm-starter/src/tests/harness.rs
+++ mini-lsm-starter/src/tests/harness.rs
@@ -1,5 +1,5 @@
-#![allow(dead_code)]
-
+#![allow(unused_variables)] // TODO(you): remove this lint after implementing this mod
+#![allow(dead_code)] // TODO(you): remove this lint after implementing this mod
 use std::{
     collections::BTreeMap, ops::Bound, os::unix::fs::MetadataExt, path::Path, sync::Arc,
     time::Duration,
